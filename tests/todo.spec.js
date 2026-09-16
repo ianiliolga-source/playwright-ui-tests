58310bd4cf84e274da26a2741e7a388f6d353934
Olga Ianishevskaia <ianiliolga@gmail.com>
2026-09-16 15:40:47 +0200

    Add completed todo test

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -8,3 +8,14 @@ test('user can add a todo item', async ({ page }) => {
 
   await expect(page.getByText('Learn Playwright')).toBeVisible();
 });
+
+test('user can complete a todo item', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/');
+
+  await page.getByPlaceholder('What needs to be done?').fill('Buy milk');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await page.getByRole('checkbox', { name: 'Toggle Todo' }).check();
+
+  await expect(page.getByText('Buy milk')).toBeVisible();
+});
